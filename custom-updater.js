bbc59c81142ef63de6925d3015419ad69945e560
蒋勇 <jiangy@highzap.com>
2022-02-20 18:35:31 +0800

    fix: 移除custom-updater内容的文件写操作

--- custom-updater.js
+++ custom-updater.js
@@ -2,10 +2,6 @@
  * @author jiangy
  * @see https://github.com/conventional-changelog/standard-version
  */
-
-const fs = require("fs");
-const path = require("path");
-
 module.exports = {
   toJson(contents) {
     let versionJson = { app: { version: undefined }, git: {} };
@@ -33,13 +29,6 @@ module.exports = {
       ? (contents.app.version = version)
       : (contents.app = { version });
 
-    // TODO: filepath 可以从 standard 中获取吗？
-    // 写入文件
-    const filepath = path.resolve(process.cwd(), "src", "manifest.json");
-    if (fs.existsSync(filepath)) {
-      fs.writeFileSync(filepath, contents);
-    } else {
-      console.error("未找到版本文件", filepath);
-    }
+    return JSON.stringify(contents);
   },
 };
