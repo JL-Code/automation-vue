48aa391184d951fa7c5504dff49104f8876f9878
蒋勇 <jiangy@highzap.com>
2022-02-20 17:00:07 +0800

    feat: 增加json-viewer

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="about">
     <h1>这是一个关于页面</h1>
-    {{ manifestJson }}
+    <json-viewer
+      :value="manifestJson"
+      :expand-depth="5"
+      copyable
+      boxed
+      sort
+    ></json-viewer>
   </div>
 </template>
 
 <script>
+import JsonViewer from "vue-json-viewer";
 import manifestJson from "@/manifest.json";
 export default {
   name: "about",
+  components: {
+    JsonViewer,
+  },
   data() {
     return {
       manifestJson,
@@ -16,3 +26,9 @@ export default {
   },
 };
 </script>
+<style lang="less" scoped>
+.version {
+  width: 800px;
+  margin: 0 auto;
+}
+</style>
